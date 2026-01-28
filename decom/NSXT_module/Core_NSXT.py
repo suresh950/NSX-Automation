f5f569cd58a32acd2a6b05edfb0f83db0128de94
Suresh Kushwaha <66522926+suresh950@users.noreply.github.com>
2026-01-28 12:12:29 +0530

    Add Core_NSXT class with Address_Group_get_APIs method

---
+++ decom/NSXT_module/Core_NSXT.py
@@ -0,0 +1,10 @@
+BASE_URL = "https://vdlmnsx01.lab.nonprod.vmware.cba"
+DOMAIN_ID = "default"
+
+class Core_NSXT():
+    def __init__(self,):
+        pass
+
+    def Address_Group_get_APIs():
+        return f'{BASE_URL}/policy/api/v1/infra/domains/{DOMAIN_ID}/groups'
+        
